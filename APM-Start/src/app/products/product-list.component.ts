4ae56ae1f523d306d21bada58287f46c6c6a44c6
aku1700 <niteesh.akurati@stateauto.com>
2019-10-01 16:46:50 -0400

    Template to Component Communication

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -9,15 +9,28 @@ import { ProductService } from './product.service';
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
-    listFilter: string;
+
     showImage: boolean;
 
     imageWidth: number = 50;
     imageMargin: number = 2;
     errorMessage: string;
 
+    private _listFilter: string
+
+    get listFilter(): string {
+      return this._listFilter
+    }
+
+    set listFilter(value: string) {
+      this._listFilter = value
+      this.performFilter(this.listFilter)
+    }
+
+
     filteredProducts: IProduct[];
     products: IProduct[];
+
 
     constructor(private productService: ProductService) { }
 
@@ -34,6 +47,7 @@ export class ProductListComponent implements OnInit {
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
+
 
     performFilter(filterBy?: string): void {
         if (filterBy) {
